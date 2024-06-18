74f903f8c8b89082e3da1efc1edf66d894427a1c
Huseyin <78041148+HsynHsyn@users.noreply.github.com>
2024-06-18 13:04:59 +0100

    Add files via upload

--- Task10_StringMethods9.java
+++ Task10_StringMethods9.java
@@ -4,18 +4,12 @@ public class Task10_StringMethods9 {
     public static void main(String[] args) {
 
         System.out.println(frontAgain("edited"));
-        System.out.println(frontAgain("edit"));
-        System.out.println(frontAgain("ed"));
     }
 
-    public static boolean frontAgain(String str) {
-
-        String first2 = str.substring(0,2);
-        String last2 = str.substring(str.length()-2);
-        return first2.equals(last2);
+    public static String frontAgain(String str) {
+        return str.substring(str.length()-2).concat(str.substring(0,str.length()-2));
     }
 }
-
 /*
 Given a string, return true if the first 2 chars in the string also appear at
 the end of the string, such as with "edited".
